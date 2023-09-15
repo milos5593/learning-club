bf6bdcaf183974ac54c3360c32cf8dbd19c077eb
iantanasijevic <ivanantanasijevicmail@gmail.com>
2023-09-15 20:28:40 +0300

    NuxtLink insted of buttons

--- task-7/components/AppHeader.vue
+++ task-7/components/AppHeader.vue
@@ -17,32 +17,31 @@ watch(() => searchInput.value, async () => {
 <template>
     <div class="bg-white border border-gray">
         <div class="container mx-auto flex justify-between py-10">
-            <!-- <div class="flex items-center"> -->
-            <div class="flex">
-                <a href="/">
-                    <SVGLogo class="mr-20" />
-                </a>
+            <div class="flex justify-center h-11">
+                <NuxtLink to="/" class="flex justify-center items-center mr-20">
+                    <SVGLogo />
+                </NuxtLink>
                 <FormKit type="search" prefix-icon="search" suffix-icon="settings" placeholder="Search something here"
                     maxlength=20 v-model="searchInput">
                 </FormKit>
             </div>
-            <div class="icons flex gap-5">
+            <div class="icons flex gap-5 h-11">
                 <NuxtLink to="/likedCars"
-                    class="p-3 border border-solid border-gray-300 rounded-full hover:border-gray-500 ease-in duration-200">
-                    <HeartIcon class="w-6" />
+                    class="flex justify-center items-center w-11 h-11 border border-solid border-gray-300 rounded-full hover:border-gray-500 ease-in duration-200">
+                    <HeartIcon class="w-6 h-6" />
                 </NuxtLink>
 
-                <button
-                    class="p-3 border border-solid border-gray-300 rounded-full hover:border-gray-500 ease-in duration-200">
-                    <BellIcon class="w-6" />
-                </button>
-                <button
-                    class="p-3 border border-solid border-gray-300 rounded-full hover:border-gray-500 ease-in duration-200">
-                    <Cog6ToothIcon class="w-6" />
-                </button>
-                <button>
-                    <img src="/images/other/profil.png">
-                </button>
+                <NuxtLink to="/"
+                    class="flex justify-center items-center w-11 h-11 border border-solid border-gray-300 rounded-full hover:border-gray-500 ease-in duration-200">
+                    <BellIcon class="w-6 h-6" />
+                </NuxtLink>
+                <NuxtLink to="/"
+                    class="flex justify-center items-center w-11 h-11 border border-solid border-gray-300 rounded-full hover:border-gray-500 ease-in duration-200">
+                    <Cog6ToothIcon class="w-6 h-6" />
+                </NuxtLink>
+                <NuxtLink to="/">
+                    <img src="/images/other/profil.png" class="h-11 w-11">
+                </NuxtLink>
             </div>
         </div>
     </div>
--- task-7/tailwind-theme.js
+++ task-7/tailwind-theme.js
@@ -10,7 +10,8 @@ export default {
         loaderIcon: 'inline-flex items-center w-4 text-gray-600 animate-spin',
         message: 'text-red-500 mb-1 text-xs',
         messages: 'list-none p-0 mt-1 mb-0',
-        outer: 'mb-4 formkit-disabled:opacity-50',
+        // outer: 'mb-4 formkit-disabled:opacity-50',
+        outer: 'formkit-disabled:opacity-50',
         prefixIcon: 'w-10 flex self-stretch grow-0 shrink-0 rounded-tl rounded-bl border-r border-gray-400 bg-white bg-gradient-to-b from-transparent to-gray-200 [&>svg]:w-full [&>svg]:max-w-[1em] [&>svg]:max-h-[1em] [&>svg]:m-auto',
         suffixIcon: 'w-10 pr-3 flex self-stretch grow-0 shrink-0 [&>svg]:w-full [&>svg]:max-w-[1em] [&>svg]:max-h-[1em] [&>svg]:m-auto'
         // suffixIcon: 'w-7 pr-3 p-3 flex self-stretch grow-0 shrink-0 [&>svg]:w-full [&>svg]:max-w-[1em] [&>svg]:max-h-[1em] [&>svg]:m-auto'
