281aae991d4f06fa62e41ad38bd06585a85181fb
iantanasijevic <ivanantanasijevicmail@gmail.com>
2023-09-16 19:04:45 +0300

    AppShowMore

--- task-7/pages/cars/index.vue
+++ task-7/pages/cars/index.vue
@@ -1,26 +1,22 @@
 <script lang="ts" setup>
 import { useCarStore } from '~/store/carStore'
 import { storeToRefs } from 'pinia';
+
 const carStore = useCarStore()
+const { filteredCars } = storeToRefs(carStore)
 await carStore.fetchCars()
-const allCars = await carStore.getAllCars
-storeToRefs(useCarStore())
+carStore.getFilteredCars
 </script>
 
 <template>
     <div class="cr__cars container mx-auto grid grid-cols-5">
-        <div class="cr__filters col-span-1">
-            <AppFilters />
-        </div>
-        <div class="cr__reservation col-span-4 p-8">
+        <AppFilters />
+        <div class="col-span-4 p-8">
             <AppReservation />
             <div class="cr__recommendationCars mt-8 grid gap-8 grid-cols-3">
-                <AppCarCard v-for="car in carStore" :car="car" />
+                <AppCarCard v-for="car in filteredCars" :car="car" />
             </div>
-            <div class="cr__showMore flex justify-center mt-16 relative">
-                <button class="text-white bg-blue-700 py-3 px-5 rounded-md">Show more car</button>
-                <p class="absolute right-0 top-3 text-gray-500">{{ carStore.totalCars }} Cars</p>
-            </div>
+            <AppShowMore :length="filteredCars.length" />
         </div>
     </div>
 </template>
